4c90bf0f02a134ca15d977dfb4177765cc6db159
shunichiiiii <shun141.0220.bbc@i.softbank.jp>
2019-12-10 18:55:13 +0900

    device-new

--- Gemfile
+++ Gemfile
@@ -54,4 +54,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'haml-rails'
 
+gem 'devise'
+
 gem 'font-awesome-sass'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  devise_for :users
 
   root "messages#index"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :update]
 end
 
   
